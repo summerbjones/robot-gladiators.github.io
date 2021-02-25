96be577db538784ea96146b570dcfe72ee3d50e1
Summer Jones <summerbjones6@gmail.com>
2021-02-25 17:17:27 -0600

    3.3.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 10;
+var playerAttack = 20;
 var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
@@ -69,6 +69,12 @@ var fight = function(enemyName) {
   }
 };
 
+//function to start a new game
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
@@ -92,4 +98,28 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
+}
+// after the loop ends, the player is either out of health or enemies to fight, so run endGame function
+
+};
+//play again
+startGame();
+// function to end the entire game 
+var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you survived the game! You now have a score of" + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost you robot in battle.");
+    }
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if(playAgainConfirm) {
+        //restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you form playing Robot Gladiators. Come back soon!");
+    }
 }
